b26c666d6fdffc5b0946e0ab464e59fbf65d4172
DishaDevalia123 <78953476+DishaDevalia123@users.noreply.github.com>
2023-10-21 08:14:28 +0530

    app.js

--- Client/app.js
+++ Client/app.js
@@ -1,1 +1,37 @@
-
+function predictChurn() {
+    var location = document.getElementById("location").value;
+    var age = parseInt(document.getElementById("age").value);
+    var subscriptionLength = parseInt(document.getElementById("sub-length").value);
+    var monthlyBill = parseFloat(document.getElementById("monthly-bill").value);
+    var totalUsage = parseFloat(document.getElementById("total-usage").value);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/predict_churn", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        displayPredictionResult(response.prediction);
+      } else {
+        displayPredictionResult("Prediction failed. Please try again.");
+      }
+    };
+  
+    var data = JSON.stringify({
+      location: location,
+      age: age,
+      subscriptionLength: subscriptionLength,
+      monthlyBill: monthlyBill,
+      totalUsage: totalUsage,
+    });
+  
+    xhr.send(data);
+  }
+  
+  function displayPredictionResult(result) {
+    var resultField = document.getElementById("predicted-result");
+    resultField.value = result;
+  }
+  
+  
